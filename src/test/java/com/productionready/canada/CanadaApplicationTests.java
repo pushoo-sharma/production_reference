744fd803480c4eca55684791799d89627bd8d91f
pushpaksharma <sharmatryhard@gmail.com>
2023-12-09 09:57:50 +0530

    Spring Boot Application with Essential Dependencies

---
+++ src/test/java/com/productionready/canada/CanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productionready.canada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
