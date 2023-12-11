d4d6bbcdaf3dff63cced7a25e375c2b4aec3b3a8
pushpaksharma <sharmatryhard@gmail.com>
2023-12-11 09:31:24 +0530

    Create Bookmark JPA entity, Spring Data JPA Repository and Service Create REST API endpoint to fetch all bookmarks Add support for pagination

---
+++ src/main/java/com/productionready/canada/domain/BookmarkRepository.java
@@ -0,0 +1,8 @@
+package com.productionready.canada.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+}
